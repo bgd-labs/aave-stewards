01790658c7ca28d2ca3d90b34376d2fa5ff060bf
Nikita <32309387+TepNik@users.noreply.github.com>
2025-02-10 23:47:46 +0800

    feat: add BatchBadDebtRepay steward contract (#4)
    Co-authored-by: TepNik <tepelin.nikita@gmail.com>
    Co-authored-by: Lukas <ls@bgdlabs.com>

---
+++ src/maintenance/interfaces/IBatchRepayBadDebtSteward.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IPool} from "aave-address-book/AaveV3.sol";
+
+import {IRescuableBase} from "solidity-utils/contracts/utils/interfaces/IRescuableBase.sol";
+
+import {IAccessControl} from "openzeppelin-contracts/contracts/access/IAccessControl.sol";
+
+interface IBatchRepayBadDebtSteward is IRescuableBase, IAccessControl {
+  /* ERRORS */
+
+  /// @notice Thrown when a user has some collateral. To repay a bad debt,
+  ///         a user should have no collateral.
+  /// @param user The address of the user
+  error UserHasSomeCollateral(address user);
+
+  /// @notice Thrown when passed address is zero
+  error ZeroAddress();
+
+  /* GLOBAL VARIABLES */
+
+  /// @notice The role that allows to call the `batchLiquidate` and `batchRepayBadDebt` functions
+  function CLEANUP_ROLE() external view returns (bytes32);
+
+  /// @notice The Aave pool
+  function POOL() external view returns (IPool);
+
+  /// @notice The Aave collector
+  function COLLECTOR() external view returns (address);
+
+  /* EXTERNAL FUNCTIONS */
+
+  /// @notice Liquidates all the users with a max debt amount to be liquidated
+  /// @param debtAsset The address of the debt asset
+  /// @param collateralAsset The address of the collateral asset that will be liquidated
+  /// @param users The addresses of the users to liquidate
+  /// @param useATokens If true the token will pull aTokens from the collector.
+  ///                   If false it will pull the underlying.
+  function batchLiquidate(address debtAsset, address collateralAsset, address[] memory users, bool useATokens) external;
+
+  /// @notice Repays all the bad debt of users
+  /// @dev Will revert if the user has a collateral or no debt.
+  ///      Repayed amount is pulled from the Aave collector. The contract sends
+  ///      any surplus back to the collector.
+  /// @param asset The address of an asset to repay
+  /// @param users The addresses of users to repay
+  /// @param useATokens If true the token will pull aTokens from the collector.
+  ///                   If false it will pull the underlying.
+  function batchRepayBadDebt(address asset, address[] calldata users, bool useATokens) external;
+
+  /// @notice Rescues the tokens
+  /// @param token The address of the token to rescue
+  function rescueToken(address token) external;
+
+  /// @notice Rescues the ETH
+  function rescueEth() external;
+
+  /* EXTERNAL VIEW FUNCTIONS */
+
+  /// @notice Returns the total debt amount and the debt amounts of users
+  /// @param asset The address of the asset to repay
+  /// @param users The addresses of the users to repay
+  /// @return totalDebtAmount The total debt amount
+  /// @return debtAmounts The debt amounts of the users
+  function getDebtAmount(address asset, address[] memory users)
+    external
+    view
+    returns (uint256 totalDebtAmount, uint256[] memory debtAmounts);
+
+  /// @notice Returns the total bad debt amount and the debt amounts of users
+  /// @dev Will revert if the user has a collateral or no debt.
+  /// @param asset The address of the asset to repay
+  /// @param users The addresses of the users to repay
+  /// @return totalDebtAmount The total debt amount
+  /// @return debtAmounts The debt amounts of the users
+  function getBadDebtAmount(address asset, address[] memory users)
+    external
+    view
+    returns (uint256 totalDebtAmount, uint256[] memory debtAmounts);
+}
---
+++ tests/gas/maintenance/BatchRepayBadDebtSteward.gas.t.sol
@@ -0,0 +1,227 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+
+import {IPool, IAToken, DataTypes} from "aave-address-book/AaveV3.sol";
+import {AaveV3Avalanche, AaveV3AvalancheAssets} from "aave-address-book/AaveV3Avalanche.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+import {IBatchRepayBadDebtSteward} from "../../../src/maintenance/interfaces/IBatchRepayBadDebtSteward.sol";
+import {BatchRepayBadDebtSteward} from "../../../src/maintenance/BatchRepayBadDebtSteward.sol";
+
+import {BatchRepayBadDebtStewardBaseTest} from "../../maintenance/BatchRepayBadDebtSteward.t.sol";
+
+contract BatchRepayBadDebtStewardGasTest is BatchRepayBadDebtStewardBaseTest {
+  function test_getBadDebtAmount_zero_users() public {
+    _callGetBadDebtAmountWithNumberOfUsers(0);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getBadDebtAmount: with 0 users");
+  }
+
+  function test_getBadDebtAmount_one_user() public {
+    _callGetBadDebtAmountWithNumberOfUsers(1);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getBadDebtAmount: with 1 user");
+  }
+
+  function test_getBadDebtAmount_two_users() public {
+    _callGetBadDebtAmountWithNumberOfUsers(2);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getBadDebtAmount: with 2 users");
+  }
+
+  function test_getBadDebtAmount_three_users() public {
+    _callGetBadDebtAmountWithNumberOfUsers(3);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getBadDebtAmount: with 2 users");
+  }
+
+  function test_getBadDebtAmount_four_users() public {
+    _callGetBadDebtAmountWithNumberOfUsers(4);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getBadDebtAmount: with 4 users");
+  }
+
+  function test_getBadDebtAmount_five_users() public {
+    _callGetBadDebtAmountWithNumberOfUsers(5);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getBadDebtAmount: with 5 users");
+  }
+
+  function test_getBadDebtAmount_six_users() public {
+    _callGetBadDebtAmountWithNumberOfUsers(6);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getBadDebtAmount: with 6 users");
+  }
+
+  function test_getDebtAmount_zero_users() public {
+    _callGetDebtAmountWithNumberOfUsers(0);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getDebtAmount: with 0 users");
+  }
+
+  function test_getDebtAmount_one_user() public {
+    _callGetDebtAmountWithNumberOfUsers(1);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getDebtAmount: with 1 user");
+  }
+
+  function test_getDebtAmount_two_users() public {
+    _callGetBadDebtAmountWithNumberOfUsers(2);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getDebtAmount: with 2 users");
+  }
+
+  function test_getDebtAmount_three_users() public {
+    _callGetDebtAmountWithNumberOfUsers(3);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getDebtAmount: with 2 users");
+  }
+
+  function test_getDebtAmount_four_users() public {
+    _callGetDebtAmountWithNumberOfUsers(4);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getDebtAmount: with 4 users");
+  }
+
+  function test_getDebtAmount_five_users() public {
+    _callGetDebtAmountWithNumberOfUsers(5);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getDebtAmount: with 5 users");
+  }
+
+  function test_getDebtAmount_six_users() public {
+    _callGetDebtAmountWithNumberOfUsers(6);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function getDebtAmount: with 6 users");
+  }
+
+  function test_batchRepayBadDebt_zero_users() public {
+    _callBatchRepayBadDebtWithNumberOfUsers(0);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchRepayBadDebt: with 0 users");
+  }
+
+  function test_batchRepayBadDebt_one_user() public {
+    _callBatchRepayBadDebtWithNumberOfUsers(1);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchRepayBadDebt: with 1 user");
+  }
+
+  function test_batchRepayBadDebt_two_users() public {
+    _callBatchRepayBadDebtWithNumberOfUsers(2);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchRepayBadDebt: with 2 users");
+  }
+
+  function test_batchRepayBadDebt_three_users() public {
+    _callBatchRepayBadDebtWithNumberOfUsers(3);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchRepayBadDebt: with 3 users");
+  }
+
+  function test_batchRepayBadDebt_four_users() public {
+    _callBatchRepayBadDebtWithNumberOfUsers(4);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchRepayBadDebt: with 4 users");
+  }
+
+  function test_batchRepayBadDebt_five_users() public {
+    _callBatchRepayBadDebtWithNumberOfUsers(5);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchRepayBadDebt: with 5 users");
+  }
+
+  function test_batchRepayBadDebt_six_users() public {
+    _callBatchRepayBadDebtWithNumberOfUsers(6);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchRepayBadDebt: with 6 users");
+  }
+
+  function test_batchLiquidate_zero_users() public {
+    _callBatchLiquidateWithNumberOfUsers(0);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchLiquidate: with 0 users");
+  }
+
+  function test_batchLiquidate_one_user() public {
+    _callBatchLiquidateWithNumberOfUsers(1);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchLiquidate: with 1 user");
+  }
+
+  function test_batchLiquidate_two_users() public {
+    _callBatchLiquidateWithNumberOfUsers(2);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchLiquidate: with 2 users");
+  }
+
+  function test_batchLiquidate_three_users() public {
+    _callBatchLiquidateWithNumberOfUsers(3);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchLiquidate: with 3 users");
+  }
+
+  function test_batchLiquidate_four_users() public {
+    _callBatchLiquidateWithNumberOfUsers(4);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchLiquidate: with 4 users");
+  }
+
+  function test_batchLiquidate_five_users() public {
+    _callBatchLiquidateWithNumberOfUsers(5);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchLiquidate: with 5 users");
+  }
+
+  function test_batchLiquidate_six_users() public {
+    _callBatchLiquidateWithNumberOfUsers(6);
+
+    vm.snapshotGasLastCall("BatchRepayBadDebtSteward", "function batchLiquidate: with 6 users");
+  }
+
+  function _callGetBadDebtAmountWithNumberOfUsers(uint256 userAmount) private view {
+    address[] memory users = new address[](userAmount);
+    for (uint256 i = 0; i < userAmount; ++i) {
+      users[i] = usersWithBadDebt[i];
+    }
+
+    steward.getBadDebtAmount(assetUnderlying, users);
+  }
+
+  function _callGetDebtAmountWithNumberOfUsers(uint256 userAmount) private view {
+    address[] memory users = new address[](userAmount);
+    for (uint256 i = 0; i < userAmount; ++i) {
+      users[i] = usersEligibleForLiquidations[i];
+    }
+
+    steward.getDebtAmount(assetUnderlying, users);
+  }
+
+  function _callBatchRepayBadDebtWithNumberOfUsers(uint256 userAmount) private {
+    address[] memory users = new address[](userAmount);
+    for (uint256 i = 0; i < userAmount; ++i) {
+      users[i] = usersWithBadDebt[i];
+    }
+
+    uint256 mintAmount = 1_000_000e18;
+    deal(assetUnderlying, collector, mintAmount);
+
+    vm.prank(guardian);
+    steward.batchRepayBadDebt(assetUnderlying, users, false);
+  }
+
+  function _callBatchLiquidateWithNumberOfUsers(uint256 userAmount) private {
+    address[] memory users = new address[](userAmount);
+    for (uint256 i = 0; i < userAmount; ++i) {
+      users[i] = usersEligibleForLiquidations[i];
+    }
+
+    uint256 mintAmount = 1_000_000e18;
+    deal(assetUnderlying, collector, mintAmount);
+
+    vm.prank(guardian);
+    steward.batchLiquidate(assetUnderlying, collateralEligibleForLiquidations, users, false);
+  }
+}
