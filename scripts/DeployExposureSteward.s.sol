aca8e18ad9cc63207d730341d68ed7b14d88e826
efecarranza <fcarr004@fiu.edu>
2025-02-28 10:49:15 -0300

    feat: deployment script

--- scripts/DeployExposureSteward.s.sol
+++ scripts/DeployExposureSteward.s.sol
@@ -6,9 +6,11 @@ import {ICollector} from "aave-v3-origin/contracts/treasury/ICollector.sol";
 import {Script, Create2Utils} from "solidity-utils/contracts/utils/ScriptUtils.sol";
 import {ChainIds} from "solidity-utils/contracts/utils/ChainHelpers.sol";
 
+import {AaveV2Polygon} from "aave-address-book/AaveV2Polygon.sol";
 import {AaveV3Polygon} from "aave-address-book/AaveV3Polygon.sol";
 import {GovernanceV3Polygon} from "aave-address-book/GovernanceV3Polygon.sol";
 
+import {AaveV2Avalanche} from "aave-address-book/AaveV2Avalanche.sol";
 import {AaveV3Avalanche} from "aave-address-book/AaveV3Avalanche.sol";
 import {GovernanceV3Avalanche} from "aave-address-book/GovernanceV3Avalanche.sol";
 
@@ -48,26 +50,26 @@ import {GovernanceV3ZkSync} from "aave-address-book/GovernanceV3ZkSync.sol";
 
 import {PoolExposureSteward} from "../src/finance/PoolExposureSteward.sol";
 
-address constant FINANCE_MULTISIG = address(0);
+address constant FINANCE_STEWARD_SAFE = address(0);
 
 library DeploymentLibrary {
   function _deploy(address admin, ICollector collector, address[] memory poolsV2, address[] memory poolsV3) internal {
     Create2Utils.create2Deploy(
       "v1",
       type(PoolExposureSteward).creationCode,
-      abi.encode(admin, FINANCE_MULTISIG, address(collector), poolsV2, poolsV3)
+      abi.encode(admin, FINANCE_STEWARD_SAFE, address(collector), poolsV2, poolsV3)
     );
   }
 
   function _deployZk(address admin, ICollector collector, address[] memory poolsV2, address[] memory poolsV3) internal {
-    new PoolExposureSteward{salt: "v1"}(admin, FINANCE_MULTISIG, address(collector), poolsV2, poolsV3);
+    new PoolExposureSteward{salt: "v1"}(admin, FINANCE_STEWARD_SAFE, address(collector), poolsV2, poolsV3);
   }
 }
 
 contract Deploy is Script {
   function deploy() internal {
     vm.startBroadcast();
-    if (block.chainid == ChainIds.MAINNET) {
+    if (ChainIds.MAINNET == block.chainid) {
       address[] memory poolsV2 = new address[](2);
       poolsV2[0] = address(AaveV2Ethereum.POOL);
       poolsV2[1] = address(AaveV2EthereumAMM.POOL);
@@ -77,38 +79,80 @@ contract Deploy is Script {
       poolsV3[2] = address(AaveV3EthereumEtherFi.POOL);
       DeploymentLibrary._deploy(GovernanceV3Ethereum.EXECUTOR_LVL_1, AaveV3Ethereum.COLLECTOR, poolsV2, poolsV3);
     }
-    if (block.chainid == ChainIds.POLYGON) {
-      //...
+
+    // Polygon
+    if (ChainIds.POLYGON == block.chainid) {
+      address[] memory poolsV2 = new address[](1);
+      poolsV2[0] = address(AaveV2Polygon.POOL);
+      address[] memory poolsV3 = new address[](1);
+      poolsV3[0] = address(AaveV3Polygon.POOL);
+      DeploymentLibrary._deploy(GovernanceV3Polygon.EXECUTOR_LVL_1, AaveV3Polygon.COLLECTOR, poolsV2, poolsV3);
     }
-    if (block.chainid == ChainIds.AVALANCHE) {
-      //...
+
+    // Avalanche
+    if (ChainIds.AVALANCHE == block.chainid) {
+      address[] memory poolsV2 = new address[](1);
+      poolsV2[0] = address(AaveV2Avalanche.POOL);
+      address[] memory poolsV3 = new address[](1);
+      poolsV3[0] = address(AaveV3Avalanche.POOL);
+      DeploymentLibrary._deploy(GovernanceV3Avalanche.EXECUTOR_LVL_1, AaveV3Avalanche.COLLECTOR, poolsV2, poolsV3);
     }
-    if (block.chainid == ChainIds.OPTIMISM) {
-      //...
+
+    address[] memory initialPoolsV2 = new address[](0);
+    address[] memory initialPoolsV3 = new address[](1);
+    if (ChainIds.OPTIMISM == block.chainid) {
+      initialPoolsV3[0] = address(AaveV3Optimism.POOL);
+      DeploymentLibrary._deploy(
+        GovernanceV3Optimism.EXECUTOR_LVL_1, AaveV3Optimism.COLLECTOR, initialPoolsV2, initialPoolsV3
+      );
     }
-    if (block.chainid == ChainIds.ARBITRUM) {
-      //...
+
+    if (ChainIds.ARBITRUM == block.chainid) {
+      initialPoolsV3[0] = address(AaveV3Optimism.POOL);
+      DeploymentLibrary._deploy(
+        GovernanceV3Optimism.EXECUTOR_LVL_1, AaveV3Optimism.COLLECTOR, initialPoolsV2, initialPoolsV3
+      );
     }
-    if (block.chainid == ChainIds.SCROLL) {
-      //...
+
+    if (ChainIds.SCROLL == block.chainid) {
+      initialPoolsV3[0] = address(AaveV3Scroll.POOL);
+      DeploymentLibrary._deploy(
+        GovernanceV3Scroll.EXECUTOR_LVL_1, AaveV3Scroll.COLLECTOR, initialPoolsV2, initialPoolsV3
+      );
     }
-    if (block.chainid == ChainIds.BASE) {
-      //...
+
+    if (ChainIds.BASE == block.chainid) {
+      initialPoolsV3[0] = address(AaveV3Base.POOL);
+      DeploymentLibrary._deploy(GovernanceV3Base.EXECUTOR_LVL_1, AaveV3Base.COLLECTOR, initialPoolsV2, initialPoolsV3);
     }
-    if (block.chainid == ChainIds.ZKSYNC) {
-      //...
+
+    if (ChainIds.BNB == block.chainid) {
+      initialPoolsV3[0] = address(AaveV3BNB.POOL);
+      DeploymentLibrary._deploy(GovernanceV3BNB.EXECUTOR_LVL_1, AaveV3BNB.COLLECTOR, initialPoolsV2, initialPoolsV3);
     }
-    if (block.chainid == ChainIds.BNB) {
-      //...
+
+    if (ChainIds.GNOSIS == block.chainid) {
+      initialPoolsV3[0] = address(AaveV3Gnosis.POOL);
+      DeploymentLibrary._deploy(
+        GovernanceV3Gnosis.EXECUTOR_LVL_1, AaveV3Gnosis.COLLECTOR, initialPoolsV2, initialPoolsV3
+      );
     }
-    if (block.chainid == ChainIds.GNOSIS) {
-      //...
+
+    if (ChainIds.METIS == block.chainid) {
+      initialPoolsV3[0] = address(AaveV3Metis.POOL);
+      DeploymentLibrary._deploy(GovernanceV3Metis.EXECUTOR_LVL_1, AaveV3Metis.COLLECTOR, initialPoolsV2, initialPoolsV3);
     }
-    if (block.chainid == ChainIds.METIS) {
-      //...
+
+    if (ChainIds.LINEA == block.chainid) {
+      initialPoolsV3[0] = address(AaveV3Linea.POOL);
+      DeploymentLibrary._deploy(GovernanceV3Linea.EXECUTOR_LVL_1, AaveV3Linea.COLLECTOR, initialPoolsV2, initialPoolsV3);
     }
-    if (block.chainid == ChainIds.LINEA) {
-      //...
+
+    if (ChainIds.ZKSYNC == block.chainid) {
+      initialPoolsV3[0] = address(AaveV3ZkSync.POOL);
+      DeploymentLibrary._deployZk(
+        GovernanceV3ZkSync.EXECUTOR_LVL_1, AaveV3ZkSync.COLLECTOR, initialPoolsV2, initialPoolsV3
+      );
     }
   }
 }
