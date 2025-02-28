602347bc7b8e1fc110fe88145b51d27d27442a0f
efecarranza <fcarr004@fiu.edu>
2025-02-28 18:55:49 -0300

    feat: add budget and update tests

--- src/finance/MainnetSwapSteward.sol
+++ src/finance/MainnetSwapSteward.sol
@@ -47,176 +47,143 @@ import {IMainnetSwapSteward} from "src/finance/interfaces/IMainnetSwapSteward.so
  * Majors: Swap wBTC to wETH as part of the treasury strategy
  *
  */
-contract MainnetSwapSteward is
-    IMainnetSwapSteward,
-    OwnableWithGuardian,
-    Multicall
-{
-    using CU for ICollector;
-    using CU for CU.SwapInput;
+contract MainnetSwapSteward is IMainnetSwapSteward, OwnableWithGuardian, Multicall {
+  using CU for ICollector;
+  using CU for CU.SwapInput;
 
-    /// @inheritdoc IMainnetSwapSteward
-    uint256 public constant MAX_SLIPPAGE = 1000; // 10%
+  /// @inheritdoc IMainnetSwapSteward
+  uint256 public constant MAX_SLIPPAGE = 1000; // 10%
 
-    /// @inheritdoc IMainnetSwapSteward
-    AaveSwapper public immutable SWAPPER;
+  /// @inheritdoc IMainnetSwapSteward
+  AaveSwapper public immutable SWAPPER;
 
-    /// @inheritdoc IMainnetSwapSteward
-    ICollector public immutable COLLECTOR;
+  /// @inheritdoc IMainnetSwapSteward
+  ICollector public immutable COLLECTOR;
 
-    /// @inheritdoc IMainnetSwapSteward
-    address public milkman;
+  /// @inheritdoc IMainnetSwapSteward
+  address public milkman;
 
-    /// @inheritdoc IMainnetSwapSteward
-    address public priceChecker;
+  /// @inheritdoc IMainnetSwapSteward
+  address public priceChecker;
 
-    /// @inheritdoc IMainnetSwapSteward
-    mapping(address fromToken => mapping(address toToken => bool isApproved))
-        public swapApprovedToken;
+  /// @inheritdoc IMainnetSwapSteward
+  mapping(address fromToken => mapping(address toToken => bool isApproved)) public swapApprovedToken;
 
-    /// @inheritdoc IMainnetSwapSteward
-    mapping(address token => address oracle) public priceOracle;
+  /// @inheritdoc IMainnetSwapSteward
+  mapping(address token => address oracle) public priceOracle;
 
-    /// @inheritdoc IMainnetSwapSteward
-    mapping(address token => uint256 budget) public tokenBudget;
+  /// @inheritdoc IMainnetSwapSteward
+  mapping(address token => uint256 budget) public tokenBudget;
 
-    constructor(
-        address initialOwner,
-        address initialGuardian,
-        address collector,
-        address swapper,
-        address initialMilkman,
-        address initialPriceChecker
-    ) OwnableWithGuardian(initialOwner, initialGuardian) {
-        _setMilkman(initialMilkman);
-        _setPriceChecker(initialPriceChecker);
+  constructor(
+    address initialOwner,
+    address initialGuardian,
+    address collector,
+    address swapper,
+    address initialMilkman,
+    address initialPriceChecker
+  ) OwnableWithGuardian(initialOwner, initialGuardian) {
+    _setMilkman(initialMilkman);
+    _setPriceChecker(initialPriceChecker);
 
-        COLLECTOR = ICollector(collector);
-        SWAPPER = AaveSwapper(swapper);
+    COLLECTOR = ICollector(collector);
+    SWAPPER = AaveSwapper(swapper);
+  }
+
+  /// @inheritdoc IMainnetSwapSteward
+  function tokenSwap(address fromToken, address toToken, uint256 amount, uint256 slippage) external onlyOwnerOrGuardian {
+    address fromOracle = priceOracle[fromToken];
+    address toOracle = priceOracle[toToken];
+
+    _validateSwap(fromToken, toToken, fromOracle, toOracle, amount, slippage);
+
+    tokenBudget[fromToken] -= amount;
+
+    COLLECTOR.swap(
+      address(SWAPPER), CU.SwapInput(milkman, priceChecker, fromToken, toToken, fromOracle, toOracle, amount, slippage)
+    );
+  }
+
+  /// @inheritdoc IMainnetSwapSteward
+  function setTokenBudget(address token, uint256 budget) external onlyOwner {
+    tokenBudget[token] = budget;
+
+    emit UpdatedTokenBudget(token, budget);
+  }
+
+  /// @inheritdoc IMainnetSwapSteward
+  function setSwappablePair(address fromToken, address toToken) external onlyOwner {
+    if (fromToken == toToken) revert UnrecognizedTokenSwap();
+
+    swapApprovedToken[fromToken][toToken] = true;
+
+    emit ApprovedToken(fromToken, toToken);
+  }
+
+  /// @inheritdoc IMainnetSwapSteward
+  function setTokenOracle(address token, address oracle) external onlyOwner {
+    if (oracle == address(0)) revert InvalidZeroAddress();
+
+    // Validate oracle has necessary functions
+    if (IAggregatorInterface(oracle).decimals() != 8) {
+      revert PriceFeedIncompatibleDecimals();
+    }
+    if (IAggregatorInterface(oracle).latestAnswer() == 0) {
+      revert PriceFeedInvalidAnswer();
     }
 
-    /// @inheritdoc IMainnetSwapSteward
-    function tokenSwap(
-        address fromToken,
-        address toToken,
-        uint256 amount,
-        uint256 slippage
-    ) external onlyOwnerOrGuardian {
-        address fromOracle = priceOracle[fromToken];
-        address toOracle = priceOracle[toToken];
+    priceOracle[token] = oracle;
 
-        _validateSwap(
-            fromToken,
-            toToken,
-            fromOracle,
-            toOracle,
-            amount,
-            slippage
-        );
+    emit SetTokenOracle(token, oracle);
+  }
 
-        tokenBudget[fromToken] -= amount;
+  /// @inheritdoc IMainnetSwapSteward
+  function setPriceChecker(address newPriceChecker) external onlyOwner {
+    _setPriceChecker(newPriceChecker);
+  }
 
-        COLLECTOR.swap(
-            address(SWAPPER),
-            CU.SwapInput(
-                milkman,
-                priceChecker,
-                fromToken,
-                toToken,
-                fromOracle,
-                toOracle,
-                amount,
-                slippage
-            )
-        );
+  /// @inheritdoc IMainnetSwapSteward
+  function setMilkman(address newMilkman) external onlyOwner {
+    _setMilkman(newMilkman);
+  }
+
+  /// @dev Internal function to set the price checker
+  function _setPriceChecker(address newPriceChecker) internal {
+    if (newPriceChecker == address(0)) revert InvalidZeroAddress();
+    address old = priceChecker;
+    priceChecker = newPriceChecker;
+
+    emit PriceCheckerUpdated(old, newPriceChecker);
+  }
+
+  /// @dev Internal function to set the Milkman instance address
+  function _setMilkman(address newMilkman) internal {
+    if (newMilkman == address(0)) revert InvalidZeroAddress();
+    address old = milkman;
+    milkman = newMilkman;
+
+    emit MilkmanAddressUpdated(old, newMilkman);
+  }
+
+  /// @dev Internal function to validate a swap's parameters
+  function _validateSwap(
+    address fromToken,
+    address toToken,
+    address fromOracle,
+    address toOracle,
+    uint256 amountIn,
+    uint256 slippage
+  ) internal view {
+    if (amountIn == 0) revert InvalidZeroAmount();
+    if (amountIn > tokenBudget[fromToken]) revert InsufficientBudget();
+    if (slippage > MAX_SLIPPAGE) revert InvalidSlippage();
+
+    if (!swapApprovedToken[fromToken][toToken]) {
+      revert UnrecognizedTokenSwap();
     }
 
-    /// @inheritdoc IMainnetSwapSteward
-    function setTokenBudget(address token, uint256 budget) external onlyOwner {
-        tokenBudget[token] = budget;
-
-        emit UpdatedTokenBudget(token, budget);
+    if (IAggregatorInterface(fromOracle).latestAnswer() == 0 || IAggregatorInterface(toOracle).latestAnswer() == 0) {
+      revert PriceFeedInvalidAnswer();
     }
-
-    /// @inheritdoc IMainnetSwapSteward
-    function setSwappablePair(
-        address fromToken,
-        address toToken
-    ) external onlyOwner {
-        if (fromToken == toToken) revert UnrecognizedTokenSwap();
-
-        swapApprovedToken[fromToken][toToken] = true;
-
-        emit ApprovedToken(fromToken, toToken);
-    }
-
-    /// @inheritdoc IMainnetSwapSteward
-    function setTokenOracle(address token, address oracle) external onlyOwner {
-        if (oracle == address(0)) revert InvalidZeroAddress();
-
-        // Validate oracle has necessary functions
-        if (IAggregatorInterface(oracle).decimals() != 8) {
-            revert PriceFeedIncompatibleDecimals();
-        }
-        if (IAggregatorInterface(oracle).latestAnswer() == 0) {
-            revert PriceFeedInvalidAnswer();
-        }
-
-        priceOracle[token] = oracle;
-
-        emit SetTokenOracle(token, oracle);
-    }
-
-    /// @inheritdoc IMainnetSwapSteward
-    function setPriceChecker(address newPriceChecker) external onlyOwner {
-        _setPriceChecker(newPriceChecker);
-    }
-
-    /// @inheritdoc IMainnetSwapSteward
-    function setMilkman(address newMilkman) external onlyOwner {
-        _setMilkman(newMilkman);
-    }
-
-    /// @dev Internal function to set the price checker
-    function _setPriceChecker(address newPriceChecker) internal {
-        if (newPriceChecker == address(0)) revert InvalidZeroAddress();
-        address old = priceChecker;
-        priceChecker = newPriceChecker;
-
-        emit PriceCheckerUpdated(old, newPriceChecker);
-    }
-
-    /// @dev Internal function to set the Milkman instance address
-    function _setMilkman(address newMilkman) internal {
-        if (newMilkman == address(0)) revert InvalidZeroAddress();
-        address old = milkman;
-        milkman = newMilkman;
-
-        emit MilkmanAddressUpdated(old, newMilkman);
-    }
-
-    /// @dev Internal function to validate a swap's parameters
-    function _validateSwap(
-        address fromToken,
-        address toToken,
-        address fromOracle,
-        address toOracle,
-        uint256 amountIn,
-        uint256 slippage
-    ) internal view {
-        if (amountIn == 0) revert InvalidZeroAmount();
-        if (amountIn > tokenBudget[fromToken]) revert InsufficientBudget();
-        if (slippage > MAX_SLIPPAGE) revert InvalidSlippage();
-
-        if (!swapApprovedToken[fromToken][toToken]) {
-            revert UnrecognizedTokenSwap();
-        }
-
-        if (
-            IAggregatorInterface(fromOracle).latestAnswer() == 0 ||
-            IAggregatorInterface(toOracle).latestAnswer() == 0
-        ) {
-            revert PriceFeedInvalidAnswer();
-        }
-    }
+  }
 }
